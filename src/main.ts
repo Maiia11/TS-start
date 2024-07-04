fbdfb73f49ddf7d05aa786c3c5009ec41502930e
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-07-04 18:05:46 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,14 @@ function add(num1: number, num2: number) {
 
 console.log(add(1, 2));
 
-const num = 10;
-const str = 'Some string';
-const bool = true;
-const empty = null;
-const notParam = undefined;
 
-function foo (num: number, str: string, bool: boolean, empty: null, notParam: undefined) {
- console.log(num, str, bool, empty);
- 
-}
 
-foo(num, str,bool, empty, notParam )
+
+let notSure: any = 4;
+notSure = 'maybe a string instead';
+notSure = false;
+notSure = {};
+
+let num: number;
+
+num = notSure;
