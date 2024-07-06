cffbc8b29d3c3e38fff2c5e102f8b4de9baf0623
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-07-06 14:57:38 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,39 @@ function add(num1: number, num2: number) {
 
 console.log(add(1, 2));
 
+function logMessage(message: string): void {
+  console.log(message);
+}
+
+logMessage('Hello, world!');
+type CallbackType = (num1: number, num2: number) => number;
+
+// Функція, яка приймає два числа та функцію зворотного виклику, застосовує цю функцію до чисел та виводить результат
+function calc(param1: number, param2: number, callback: CallbackType): void {
+  console.log('Result:', callback(param1, param2));
+}
+
+// Приклади використання calc з різними функціями зворотного виклику
+calc(1, 1, (num1, num2) => num1 + num2);
+calc(10, 5, (num1, num2) => num1 - num2);
+
+interface Walkable {
+  walk(): void;
+}
+
+interface Eatable {
+  eat(): void;
+}
+
+class Animal implements Walkable, Eatable {
+  walk() {
+    console.log('The animal walks...');
+  }
+
+  eat() {
+    console.log('The animal eats...');
+  }
+}
+
+const animal = new Animal();
+console.log(animal);
